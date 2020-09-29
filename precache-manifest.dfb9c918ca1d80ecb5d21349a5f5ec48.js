d84f474426dbb33135287bb2a71a22847c175cb8
Evelina Björkman <evelina.v.bjorkman@gmail.com>
2020-09-29 10:11:02 +0200

    Updates

---
+++ precache-manifest.dfb9c918ca1d80ecb5d21349a5f5ec48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac4524360e3a718c479b3b200acc075",
+    "url": "/js-lesson-deploy-2/index.html"
+  },
+  {
+    "revision": "0e2ec4d944b5fcb445d0",
+    "url": "/js-lesson-deploy-2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0112e5c36cab101c5a2c",
+    "url": "/js-lesson-deploy-2/static/js/2.7888c1c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-lesson-deploy-2/static/js/2.7888c1c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e2ec4d944b5fcb445d0",
+    "url": "/js-lesson-deploy-2/static/js/main.2f2d268b.chunk.js"
+  },
+  {
+    "revision": "5c9d2e3707f2fdfcb58c",
+    "url": "/js-lesson-deploy-2/static/js/runtime-main.78888a72.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/js-lesson-deploy-2/static/media/logo.5d5d9eef.svg"
+  }
+]);
