90ae4566d4512a351bc84c06d1e9cb4888c956ef
Evelina Björkman <evelina.v.bjorkman@gmail.com>
2020-09-29 11:16:33 +0200

    Fixed router problem

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrouserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrouserRouter>
+    <BrowserRouter>
       <App />
-    </BrouserRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
